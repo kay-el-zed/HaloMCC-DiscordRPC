ed779500911b6f076ebef3f6ac978f58dced6bd6
kay el zed <me@klzed.net>
2019-12-15 20:05:32 -0400

    first version. checks for online status and displays rich presence from XBL as Discord status

--- Halo MCC Rich Presence/Halo_MCC_Rich_Presence.py
+++ Halo MCC Rich Presence/Halo_MCC_Rich_Presence.py
@@ -5,6 +5,8 @@ from pypresence import Presence
 import sys, time
 
 discord_client_id = 655596739138551848
+xbox_status_last_change = None
+discord_online_status = False
 
 ## Handle XBL API authentication.
 try:
@@ -15,7 +17,6 @@ except FileNotFoundError as e:
     'ERROR: {}'.format(e.filename, e.strerror)
   )
   sys.exit(-1)
-
 try:
   auth_mgr.authenticate(do_refresh=True)
 except AuthenticationException as e:
@@ -26,17 +27,34 @@ xbl_client = XboxLiveClient(auth_mgr.userinfo.userhash, auth_mgr.xsts_token.jwt,
 
 ## discord RPC init 
 discordRPC = Presence(discord_client_id)
-discordRPC.connect()
-discordRPC.update(large_image='cover', large_text='Halo: MCC',
-           small_image='win10', small_text="Playing on PC")
-
 
 ## Discord RPC update loop
 while True:
     try:
+        ##get current presence
         XboxPresence = xbl_client.presence.get_presence_own().json()
-        print ("Current Presence: ", XboxPresence)
+        ##If we're online, do all this
+        if (XboxPresence['state'] == 'Online'):
+            OnlineDevices = XboxPresence['devices']
+            for ActiveTitles in OnlineDevices:
+                ##check if we're actually playing MCC
+                if ActiveTitles['titles'][0]['name'] == "Halo: The Master Chief Collection":
+                    print("We're playing something! Lets's start up Discord RPC")
+                    ##start Discord RPC
+                    if discord_online_status == False:
+                        discordRPC.connect()
+                        discord_online_status = True
+                    ##set the current status
+                    DiscordStatus = ActiveTitles['titles'][0]['activity']['richPresence'].split(" - ")
+                    discordRPC.update(large_image='cover', large_text='Halo: MCC', small_image='win10', small_text="Playing on PC", state=DiscordStatus[0], details=DiscordStatus[1])
+                    break
+                else:
+                    print("Guess we're not playing anything. Close Discord RPC if active!")
+                    if discord_online_status == True:
+                        discordRPC.close()
+                        discord_online_status == False
         time.sleep(15)
     except:
-        RPC.close()
+        print("Crashed or received KeyboardInterrupt! Closing Discord RPC and shutting down script...")
+        discordRPC.close()
         sys.exit(0)
