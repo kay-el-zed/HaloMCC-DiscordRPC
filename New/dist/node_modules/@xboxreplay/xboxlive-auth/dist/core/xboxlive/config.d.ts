538c5a6c275963b9d8f3d4275f502bdf7852b709
Gurrman375 <james.gurr375@gmail.com>
2021-05-17 11:32:00 -0700

    fixed issues with rpc client

---
+++ New/dist/node_modules/@xboxreplay/xboxlive-auth/dist/core/xboxlive/config.d.ts
@@ -0,0 +1,9 @@
+export declare const defaultXSTSRelyingParty = "http://xboxlive.com";
+declare const _default: {
+    urls: {
+        userAuthenticate: string;
+        deviceAuthenticate: string;
+        XSTSAuthorize: string;
+    };
+};
+export default _default;
---
+++ New/node_modules/@xboxreplay/xboxlive-auth/dist/utils/index.d.ts
@@ -0,0 +1,8 @@
+export declare const getBaseHeaders: (additionalHeaders?: Record<string, string>) => {
+    Pragma: string;
+    Accept: string;
+    'User-Agent': string;
+    'Cache-Control': string;
+    'Accept-Encoding': string;
+    'Accept-Language': string;
+};
